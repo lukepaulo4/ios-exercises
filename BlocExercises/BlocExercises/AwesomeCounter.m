48b027247793cabb082200c35f80aca0ad3b96d5
Luke Paulo <lukepaulo4@gmail.com>
2016-05-27 13:01:29 -0700

    Assignment 12 done

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,29 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    
+    NSInteger lowNumber;
+    NSInteger highNumber;
+    NSString *equalNumber;
+    
+    if (number < otherNumber) {
+        lowNumber = number;
+        highNumber = otherNumber;
+    } else if (otherNumber < number) {
+        highNumber = number;
+        lowNumber = otherNumber;
+    } else if (number == otherNumber) {
+        equalNumber = [NSString stringWithFormat:@"%ld", (long)lowNumber];
+    }
+    
+    NSMutableString *output = [NSMutableString string];
+    
+    for (NSInteger i = lowNumber; i <= highNumber; i++) {
+        [output appendFormat:@"%ld", i];
+    }
+    
+    
+    return output;
 }
 
 @end
