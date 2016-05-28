ecbdbab85b108fe603e59b46e446118529d2716a
Luke Paulo <lukepaulo4@gmail.com>
2016-05-28 16:31:58 -0700

    Assignment 13 DONE

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -10,24 +10,51 @@
 
 @implementation StarTrekArrays
 
+
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    
+    // The noted out code is what you originally had. Wrong! Don't need to create all this stuff, as it has already been done for you in the other method! Simplify your life one time...
+    
+    //characterString = @"Worf, son of Mogh, slayer of Gowron; Captain Jean-Luc Picard of the USS Enterprise; Beverly Crusher, Chief Medical Officer";
+    //NSArray *charArray = [characterString componentsSeparatedByString:@"; "];
+    //[charArray enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+    
+    NSArray *charArray = [characterString componentsSeparatedByString:@";"];
+        return charArray;
+    
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    
+    NSString *array2String = [characterArray componentsJoinedByString:@";"];
+    
+    return array2String;
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *charMuter = [characterArray mutableCopy];
+    NSSortDescriptor *sortCharMuter = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+    [charMuter sortUsingDescriptors:@[sortCharMuter]];
+    
+    
+    return charMuter;
+    
+    
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
-    return NO;
+    
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'Worf'"];
+    [characterArray filteredArrayUsingPredicate:containsWorf];
+    
+    
+    return YES;
+
 }
 
 @end
