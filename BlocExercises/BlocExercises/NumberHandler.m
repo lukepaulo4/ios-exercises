4fdf7e5b6ee4514eca44b4b0b11300d526570e2d
Luke Paulo <lukepaulo4@gmail.com>
2016-05-30 00:37:59 -0700

    Assignment 15 Completed

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -12,17 +12,53 @@
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
     /* WORK HERE */
-    return @0;
+    int doubleNumber = [number intValue] * 2;
+    NSNumber *doubled = [NSNumber numberWithInt:doubleNumber];
+    
+    return doubled;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @[];
+    if (number == otherNumber) {
+        return @[@(number)];
+    }
+    
+    NSInteger lowNumber;
+    NSInteger highNumber;
+    
+    if (number < otherNumber) {
+        lowNumber = number;
+        highNumber = otherNumber;
+    } else {
+        lowNumber = otherNumber;
+        highNumber = number;
+    }
+    
+    NSMutableArray *mfinArray = [NSMutableArray array];
+    
+    for (NSInteger i = lowNumber; i <= highNumber; i++) {
+        [mfinArray addObject:@(i)];
+        
+    }
+    
+    return mfinArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
     /* WORK HERE */
-    return 0;
+    NSInteger littleGuy = NSIntegerMax;
+    
+    for (NSNumber *number in arrayOfNumbers) {
+        NSInteger numInArray = [number integerValue];
+        
+        if (numInArray < littleGuy) {
+            littleGuy = numInArray;
+        }
+    }
+    
+    return littleGuy;
+    
 }
 
 @end
