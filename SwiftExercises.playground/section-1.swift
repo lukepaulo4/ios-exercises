6589d3359d9d62be84d618e8aa1027863f4f88e0
Luke Paulo <lukepaulo4@gmail.com>
2016-06-01 23:01:35 -0700

    Checkpoint 18 complete

--- SwiftExercises.playground/section-1.swift
+++ SwiftExercises.playground/section-1.swift
@@ -8,11 +8,13 @@ Strings
 
 func favoriteCheeseStringWithCheese(cheese: String) -> String {
     // WORK HERE
-    return cheese
+   
+    return "My favorite cheese is " + cheese + "."
 }
 
 let fullSentence = favoriteCheeseStringWithCheese("cheddar")
 // Make fullSentence say "My favorite cheese is cheddar."
+
 
 /*
 
@@ -21,10 +23,20 @@ Arrays & Dictionaries
 */
 
 let numberArray = [1, 2, 3, 4]
+let newArray = numberArray + [5]
+
+//OR change 'let numberArray' to a var to make it changeable!
+
 // Add 5 to this array
 // WORK HERE
 
-let numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+var numberDictionary = [1 : "one", 2 : "two", 3 : "three", 4 : "four"]
+numberDictionary[5] = "five"
+print(numberDictionary)
+
+
+//^^Swift dictionaries are random like Obj-C, so I think this is good?
+
 // Add 5 : "five" to this dictionary
 // WORK HERE
 
@@ -37,8 +49,21 @@ Loops
 // Use a closed range loop to print 1 - 10, inclusively
 // WORK HERE
 
+
+
+for numb in 1...10 {
+    print("\(numb)")
+}
+
 // Use a half-closed range loop to print 1 - 10, inclusively
 // WORK HERE
+
+for numb in 1..<10 {
+    print("\(numb)")
+}
+
+
+
 
 let worf = [
     "name": "Worf",
@@ -58,7 +83,16 @@ let characters = [worf, picard]
 func favoriteDrinksArrayForCharacters(characters:[[String : String]]) -> [String] {
     // return an array of favorite drinks, like ["prune juice", "tea, Earl Grey, hot"]
     // WORK HERE
-    return []
+    
+    var favDrinksSon = [String]()
+ 
+    for character in characters {
+        if let drink = character["favorite drink"] {
+            favDrinksSon.append(drink)
+        }
+    }
+    
+    return favDrinksSon
 }
 
 let favoriteDrinks = favoriteDrinksArrayForCharacters(characters)
@@ -73,9 +107,16 @@ Optionals
 
 func emailFromUserDict(userDict : [String : String]) -> String {
     // Return the user's email address from userDict, or return "" if they don't have one
-    
     // WORK HERE
-    return "user@example.com"
+    
+    let email = userDict["email"]
+    
+    if email != nil {
+        let unwrappedEmail = email!
+        return unwrappedEmail
+    } else {
+        return ""
+    }
 }
 
 
@@ -100,8 +141,21 @@ Functions
 let strings = ["milk", "eggs", "bread", "challah"]
 
 // WORK HERE - make your function and pass `strings` in
+//var inputArray = ["milk", "eggs", "bread", "challah"]
 
-let expectedOutput = "milk;eggs;bread;challah"
+func outputFoodStuff(inputArray:Array<String>) -> String {
+    var outNow = ""
+    for inputNow in inputArray {
+        outNow += ";\(inputNow)"
+    }
+    
+    return outNow
+
+}
+
+  let expectedOutput = outputFoodStuff(strings)
+
+//let expectedOutput = "milk;eggs;bread;challah"
 
 /*
 
@@ -110,6 +164,8 @@ Closures
 */
 
 let cerealArray = ["Golden Grahams", "Cheerios", "Trix", "Cap'n Crunch OOPS! All Berries", "Cookie Crisp"]
+var cerealSortage = cerealArray
 
-// Use a closure to sort this array alphabetically
-// WORK HERE
+var sortedCereally = cerealSortage.sort()
+
+
